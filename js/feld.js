1cec9eb2c4a7aeca6415e28785413fb70b320430
Feld Giesy (admin) <feld.giesy@bixal.com>
2018-02-28 15:20:35 -0500

    I am not good at jQuery.

--- js/feld.js
+++ js/feld.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
   jQuery('.main-content').find('.accordion-trigger').click(function(){
 
     //Expand or collapse this panel
-    jQuery(this).next().slideToggle('fast');
+    jQuery(this).next('.accordion-content').slideToggle('fast');
 
     //Hide the other panels
     jQuery(".accordion-content").not($(this).next()).slideUp('fast');
