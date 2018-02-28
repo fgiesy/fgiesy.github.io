52fb7bb5b0371c23cd18b2420a0674c3b4fdd605
Feld Giesy (admin) <feld.giesy@bixal.com>
2018-02-28 15:31:10 -0500

    oh boy, jQuery, you unwieldy beast.

--- js/feld.js
+++ js/feld.js
@@ -2,10 +2,10 @@ jQuery(document).ready(function($) {
   jQuery('.main-content').find('.accordion-trigger').click(function(){
 
     //Expand or collapse this panel
-    jQuery(this).next('.accordion-content').slideToggle('fast');
+    jQuery(this).parent().closest('.accordion-content').slideToggle('fast');
 
     //Hide the other panels
-    jQuery(".accordion-content").not($(this).next()).slideUp('fast');
+    jQuery(".accordion-content").not($(this).parent().closest('.accordion-content')).slideUp('fast');
 
   });
 });
