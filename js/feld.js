e6615dbe1dac917605ef8db08f72ff3687decfdd
Feld Giesy (admin) <feld.giesy@bixal.com>
2018-02-28 15:32:40 -0500

    Doing it a different way.

--- js/feld.js
+++ js/feld.js
@@ -2,10 +2,10 @@ jQuery(document).ready(function($) {
   jQuery('.main-content').find('.accordion-trigger').click(function(){
 
     //Expand or collapse this panel
-    jQuery(this).parent().closest('.accordion-content').slideToggle('fast');
+    $(this).parent().closest('.accordion-content').addClass('accordion-expanded');
 
     //Hide the other panels
-    jQuery(".accordion-content").not($(this).parent().closest('.accordion-content')).slideUp('fast');
+   
 
   });
 });
