1868bdb1d064efefda0b578c4cc4b153ec565136
Feld Giesy (admin) <feld.giesy@bixal.com>
2018-03-01 09:36:56 -0500

    Heading extends and js updates.

--- js/feld.js
+++ js/feld.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
   jQuery('.main-content').find('.accordion-trigger').click(function(){
 
     //Expand or collapse this panel
-    $(this).parent().closest('.accordion-content').addClass('accordion-expanded');
+    $(this).parent().sibling('.accordion-content').addClass('accordion-expanded');
 
     //Hide the other panels
    
