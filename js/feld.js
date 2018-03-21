eb79280b350f26f58a7606c6986d3b6ef2be3b7b
Feld Giesy <feldgiesy@gmail.com>
2018-03-21 12:18:06 -0400

    Text styles for the job part of my resume.

--- js/feld.js
+++ js/feld.js
@@ -1,11 +1,17 @@
 jQuery(document).ready(function($) {
+  
+  //basic accordions
   jQuery('.main-content').find('.accordion-trigger, .hide-resume').click(function(){
-
     //Expand or collapse this panel
     $(this).toggleClass('accordion-trigger-active');
     $(this).parent().siblings('.accordion-content').toggleClass('accordion-expanded');
-
     //Hide the other panels
 
   });
+  
+  //hiding resume
+  jQuery('.hide-resume').click(function(){
+    $(this).parent('.accordion-content').removeClass('accordion-expanded');
+  });
+
 });
